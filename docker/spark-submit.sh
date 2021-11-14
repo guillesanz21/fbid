0509a85c633bac7590ce9e05ce2063ffaaefddd7
JaimeSaez97 <j.saezdeburuaga@upm.es>
2021-11-14 19:32:30 +0100

    Funcionan 3 primeros puntos

--- docker/spark-submit.sh
+++ docker/spark-submit.sh
@@ -2,4 +2,4 @@
 
 cd /opt/spark/
 
-./bin/spark-submit --class es.upm.dit.ging.predictor.MakePrediction --master spark://guille:7077  --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /jar/flight_prediction_2.12-0.1.jar
+./bin/spark-submit --class es.upm.dit.ging.predictor.MakePrediction --master spark://kitt:7077  --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /jar/flight_prediction_2.12-0.1.jar
